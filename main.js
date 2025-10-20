a9c531b2804fa76d930c06d048ae85cca819c877
Steffen Aichele <steffen.aichele@icloud.com>
2025-10-20 12:21:03 +0200

    refactored generations object as array

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ import addSections from "/createSections.js";
 const path = "https://pokeapi.co/api/v2/pokemon/";
 let counter = 1;
 
-const generations = {
-    gen1: { start: 1, end: 151 },
-    gen2: { start: 152, end: 251 },
-    gen3: { start: 252, end: 386 },
-    gen4: { start: 387, end: 493 },
-    gen5: { start: 494, end: 649 },
-    gen6: { start: 650, end: 721 },
-    gen7: { start: 722, end: 809 },
-    gen8: { start: 810, end: 905 },
-    gen9: { start: 906, end: 1025 }
-};
+const generations = [
+    {name:"Generation 1", start: 1, end: 151 },
+    {name:"Generation 2", start: 152, end: 251 },
+    {name:"Generation 3", start: 252, end: 386 },
+    {name:"Generation 4", start: 387, end: 493 },
+    {name:"Generation 5", start: 494, end: 649 },
+    {name:"Generation 6", start: 650, end: 721 },
+    {name:"Generation 7", start: 722, end: 809 },
+    {name:"Generation 8", start: 810, end: 905 },
+    {name:"Generation 9", start: 906, end: 1025 }
+];
 
 for (const gen in generations) {
     addSections(gen);
